c2cc301a38c2846ba6422c1f9d352bc35e1847d1
Kaique Beraguas <ksakX@outlook.com>
2023-09-18 02:08:04 -0300

    Monitoramento

--- src/main/java/br/com/example/api/controllers/ContagemController.java
+++ src/main/java/br/com/example/api/controllers/ContagemController.java
@@ -6,18 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.annotation.Counted;
 import io.swagger.annotations.Api;
 
 @Api(tags = "Api de contagem")
 @RestController
 @RequestMapping("/api/v1/contagem")
 public class ContagemController {
-    
+
     private static int contador = 0;
 
+    @Timed(value = "api_contagem")
+    @Counted(recordFailuresOnly = false, value = "Teste")
     @GetMapping
-    public ResponseEntity<Integer> get(){
-         return new ResponseEntity<Integer>(contador++,HttpStatus.OK);
+    public ResponseEntity<Integer> get() {
+        return new ResponseEntity<Integer>(contador++, HttpStatus.OK);
     }
-    
+
 }
